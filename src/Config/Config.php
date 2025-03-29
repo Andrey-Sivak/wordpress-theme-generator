831c9358a8235024d0f30e8135936b4335ccdf08
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-03-30 00:17:24 +0200

    feat(config): add Config class with author constants
    - Created src/Config/config.php with AUTHOR_NAME and AUTHOR_GITHUB_URL

---
+++ src/Config/Config.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+namespace WPTG\Config;
+
+class Config
+{
+    public const string AUTHOR_NAME = 'Andrey-Sivak';
+    public const string AUTHOR_GITHUB_URL = 'https://github.com/Andrey-Sivak';
+}

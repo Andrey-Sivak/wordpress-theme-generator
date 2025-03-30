503660306d0715913daab8c173fe8fafbd4b241c
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-03-30 15:29:21 +0300

    chore(handler): add php doc for footer.php

--- src/Handlers/FooterFileHandler.php
+++ src/Handlers/FooterFileHandler.php
@@ -11,6 +11,15 @@ class FooterFileHandler implements FileHandler {
     ): string {
         return <<<PHP
 <?php
+/**
+ * The footer template for the theme
+ *
+ * This file contains the closing body and HTML tags, along with footer content
+ * such as widgets, copyright info, and scripts. It is included via get_footer().
+ *
+ * @package {themeName}
+ */
+ 
 declare(strict_types=1);
 ?>
 <footer id="{$textDomain}-footer" class=""></footer>

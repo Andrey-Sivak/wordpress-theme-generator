41abf272869163cddd760b184edf5bc59b3b4b46
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-03-30 15:30:32 +0300

    chore(handler): update spaces for functions.php

--- src/Handlers/FunctionsFileHandler.php
+++ src/Handlers/FunctionsFileHandler.php
@@ -40,38 +40,38 @@ define('_{$textDomainUpper}_VERSION', '1.0.0');
  * as indicating support for post thumbnails.
  */
 function {$themeName}_setup(): void {
-    // Make theme available for translation
-    load_theme_textdomain('{$textDomain}', get_template_directory() . '/languages');
+	// Make theme available for translation.
+	load_theme_textdomain( '{$textDomain}', get_template_directory() . '/languages' );
 
-    // Add default posts and comments RSS feed links to head
-    add_theme_support('automatic-feed-links');
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
 
-    // Let WordPress manage the document title
-    add_theme_support('title-tag');
+	// Let WordPress manage the document title.
+	add_theme_support( 'title-tag' );
 
-    // Enable support for Post Thumbnails
-    add_theme_support('post-thumbnails');
+	// Enable support for Post Thumbnails.
+	add_theme_support( 'post-thumbnails' );
 
-    // Register navigation menus
-    register_nav_menus(
-        array(
-            'menu-1' => esc_html__('Primary', '{$textDomain}'),
-        ),
-    );
+	// Register navigation menus.
+	register_nav_menus(
+		array(
+			'menu-1' => esc_html__( 'Primary', '{$textDomain}' ),
+		),
+	);
 
-    // Switch core markup to HTML5
-    add_theme_support(
-        'html5',
-        array(
-            'search-form',
-            'comment-form',
-            'comment-list',
-            'gallery',
-            'caption',
-            'style',
-            'script',
-        ),
-    );
+	// Switch core markup to HTML5.
+	add_theme_support(
+		'html5',
+		array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+			'style',
+			'script',
+		),
+	);
 }
 add_action('after_setup_theme', '{$themeName}_setup');
 
@@ -81,35 +81,35 @@ add_action('after_setup_theme', '{$themeName}_setup');
  * @return string
  */
 function {$themeName}_get_ajax_url(): string {
-    return get_template_directory_uri() . '/inc/front-ajax.php';
+	return get_template_directory_uri() . '/inc/front-ajax.php';
 }
 
 /**
  * Enqueue scripts and styles.
  */
 function {$themeName}_scripts(): void {
-    \$ajax_url = {$themeName}_get_ajax_url();
+	\$ajax_url = {$themeName}_get_ajax_url();
 
-    \${$themeName}_options = [
-        'ajax_url' => \$ajax_url,
-        'home_url' => get_home_url(),
-    ];
+	\${$themeName}_options = array(
+		'ajax_url' => \$ajax_url,
+		'home_url' => get_home_url(),
+	);
 
-    wp_dequeue_style('select2');
-    wp_dequeue_script('select2');
-    wp_deregister_script('select2');
+	wp_dequeue_style( 'select2' );
+	wp_dequeue_script( 'select2' );
+	wp_deregister_script( 'select2' );
 
-    wp_dequeue_script('jquery');
-    wp_deregister_script('jquery');
+	wp_dequeue_script( 'jquery' );
+	wp_deregister_script( 'jquery' );
 
-    wp_enqueue_style('{$themeName}-style', get_stylesheet_uri(), array(), _{$textDomainUpper}_VERSION);
-    wp_enqueue_style('{$themeName}-main-style', get_template_directory_uri() . '/dist/css/style.min.css', [], _{$textDomainUpper}_VERSION);
-    wp_enqueue_style('{$themeName}-fonts-style', get_template_directory_uri() . '/fonts/{$themeName}-fonts.css', [], _{$textDomainUpper}_VERSION);
+	wp_enqueue_style( '{$themeName}-style', get_stylesheet_uri(), array(), _{$textDomainUpper}_VERSION );
+	wp_enqueue_style( '{$themeName}-main-style', get_template_directory_uri() . '/dist/css/style.min.css', array(), _{$textDomainUpper}_VERSION );
+	wp_enqueue_style( '{$themeName}-fonts-style', get_template_directory_uri() . '/fonts/{$themeName}-fonts.css', array(), _{$textDomainUpper}_VERSION );
 
-    wp_enqueue_script('{$themeName}-main-script', get_template_directory_uri() . '/dist/js/app.min.js', [], _{$textDomainUpper}_VERSION, true);
-    wp_localize_script('{$themeName}-main-script', 'options', \${$themeName}_options);
+	wp_enqueue_script( '{$themeName}-main-script', get_template_directory_uri() . '/dist/js/app.min.js', array(), _{$textDomainUpper}_VERSION, true );
+	wp_localize_script( '{$themeName}-main-script', 'options', \${$themeName}_options );
 }
-add_action('wp_enqueue_scripts', '{$themeName}_scripts');
+add_action( 'wp_enqueue_scripts', '{$themeName}_scripts' );
 PHP;
     }
 }

780b7c5ae8ba6a959917ae86d5422a1a1391f88c
Andrey-Sivak <andrey.sivak.dev@gmail.com>
2025-03-30 15:29:09 +0300

    chore(handler): add php doc for header.php

--- src/Handlers/HeaderFileHandler.php
+++ src/Handlers/HeaderFileHandler.php
@@ -11,6 +11,15 @@ class HeaderFileHandler implements FileHandler {
     ): string {
         return <<<PHP
 <?php
+/**
+ * The header template for the theme
+ *
+ * This file contains the HTML head section, opening body tag, and top-level site markup
+ * such as the site header and navigation. It is included in all front-end pages via get_header().
+ *
+ * @package {themeName}
+ */
+ 
 declare(strict_types=1);
 
 \${$themeName}_body_class = '';
@@ -19,15 +28,15 @@ declare(strict_types=1);
 <!doctype html>
 <html <?php language_attributes(); ?>>
 <head>
-    <meta charset="<?php bloginfo('charset'); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
-<body <?php body_class(\${$themeName}_body_class); ?>>
+<body <?php body_class( \${$themeName}_body_class ); ?>>
 <?php wp_body_open(); ?>
-     <header id="{$textDomain}-header" class=""></header>
+	<header id="ew-header" class=""></header>
 PHP;
     }
 }
